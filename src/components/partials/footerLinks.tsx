0a8ce1937cb306c19a704153fc8d35e82fe167d8
Obed Nuertey <onuertey1997@gmail.com>
2023-09-26 13:11:26 +0000

    feat: added social links

--- src/components/partials/footerLinks.tsx
+++ src/components/partials/footerLinks.tsx
@@ -3,10 +3,10 @@ import FooterLinks from "../namespaces/FooterLinks";
 export default ({className}:FooterLinks.IfooterLink<string, number>):JSX.Element=>{
 
     const linksArray:FooterLinks.IfooterLink<string, number>[] = [
-        {className, href: "", linkText: "Twitter", rel:"noreferrer", target: "_blank"},
-        {className, href: "", linkText: "GitHub", rel:"noreferrer", target: "_blank"},
-        {className, href: "", linkText: "LinkedIn", rel:"noreferrer", target: "_blank"},
-        {className, href: "", linkText: "Email", rel:"noreferrer", target: "_blank"}
+        {className, href: "https://twitter.com/obedNuertey1", linkText: "Twitter", rel:"noreferrer", target: "_blank"},
+        {className, href: "https://github.com/obedNuertey1", linkText: "GitHub", rel:"noreferrer", target: "_blank"},
+        {className, href: "https://www.linkedin.com/in/obed-nuertey-60b63b227/", linkText: "LinkedIn", rel:"noreferrer", target: "_blank"},
+        {className, href: "mailto:onuertey1997@gmail.com?subject=Email from your website", linkText: "Email", rel:"noreferrer", target: "_blank"}
     ];
 
     const footerLinksObject = new FooterLinks.FooterLinkClass<FooterLinks.IfooterLink<string, number>>(linksArray);
