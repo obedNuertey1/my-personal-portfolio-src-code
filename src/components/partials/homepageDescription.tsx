e7c702ac03ed0903b77b3b9e02afc3c74814383d
Obed Nuertey <onuertey1997@gmail.com>
2023-10-09 13:37:07 +0000

    feat: modified homepageDescription.tsx

--- src/components/partials/homepageDescription.tsx
+++ src/components/partials/homepageDescription.tsx
@@ -1,6 +1,6 @@
 import PageDescriber from "../namespaces/PageDescriber";
 
-export default ({className, id}:any):JSX.Element => {
+export default ({className, id}:PageDescriber.IpageDescriber<string>):JSX.Element => {
     const description:PageDescriber.IpageDescriber<string> = {
         heading: "Hello, my name is Obed.",
         paragraph: "I am a Software Engineer based in Accra, Ghana. Currently I'm working on some personal projects",
