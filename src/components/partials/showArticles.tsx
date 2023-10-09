f0b6623c7b9e93c79d9bc3c6480ea0f4147b1675
Obed Nuertey <onuertey1997@gmail.com>
2023-10-09 14:37:06 +0000

    feat: added showArticles.tsx partial

--- src/components/partials/showArticles.tsx
+++ src/components/partials/showArticles.tsx
@@ -1,13 +1,16 @@
-// import Articles from "../namespaces/Articles";
+import Articles from "../namespaces/Articles";
 
-// export default ({className, id}:Articles.Iarticle<string, Articles.IcssId<string>, Articles.IcssClass>):JSX.Element => {
-//     const myArticles:Articles.Iarticle<string, Articles.IcssClass, Articles.IcssId<string>>[] = [
-//         {className, id, time: "10/4/2022", $h3: "Interesting AI Articles & Links"},
-//         {className, id, time: "10/4/2022", $h3: "Interesting AI Articles & Links"},
-//     ];
+export default ({className, id}:Articles.Iarticle<string, Articles.IcssId<string>, Articles.IcssClass>):JSX.Element => {
+    const myArticles:Articles.Iarticle<string, Articles.IcssClass, Articles.IcssId<string>>[] = [
+        {className, id, time: "10/4/2022", $h3: "Interesting AI Articles & Links"},
+        {className, id, time: "10/4/2022", $h3: "Interesting AI Articles & Links"},
+    ];
 
-//     return (
-//         <>
-//         </>
-//     );
-// };
+    const articlesInstance = new Articles.ArticlesClass<Articles.Iarticle<string, Articles.IcssId<string>, Articles.IcssClass>>(myArticles);
+
+    return (
+        <>
+            {articlesInstance}
+        </>
+    );
+};
