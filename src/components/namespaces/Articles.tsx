ca7d0c7f6bb6d7c17bd4c377e978359f24eb2b48
Obed Nuertey <onuertey1997@gmail.com>
2023-10-09 14:20:14 +0000

    feat: added a namespace for articles

---
+++ src/components/namespaces/Articles.tsx
@@ -0,0 +1,42 @@
+namespace Articles {
+
+    export interface IcssId<S>{
+        parent: S;
+        articleTime: S;
+        articleTopic: S;
+    }
+
+    export interface IcssClass extends IcssId<string>{};
+
+    export interface Iarticle<S, U extends IcssId<string>, T extends IcssClass>{
+        time?: S;
+        $h3?: S;
+        id: U;
+        className: T;
+    };
+
+    export type articleJSX = JSX.Element[];
+
+    export class ArticlesClass<U extends Iarticle<string, IcssId<string>, IcssClass>>{
+        private articlesArray: U[];
+
+        constructor(articlesArray:U[]){
+            this.articlesArray = articlesArray;
+        }
+
+        public getArticles():articleJSX{
+            return this.articlesArray.map((elem:U):JSX.Element => {
+                return (
+                    <>
+                        <article className={elem.className.parent}>
+                            <time id={elem.id.articleTime} className={elem.className.articleTime}>{elem.time}</time>
+                            <h3 id={elem.id.articleTopic} className={elem.className.articleTopic}>{elem.$h3}</h3>
+                        </article>
+                    </>
+                );
+            });
+        }
+    };
+
+};
+export default Articles;
