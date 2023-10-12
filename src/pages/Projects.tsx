e8555387f3018342ab7a9db9f79b0a8efdb1a876
Obed Nuertey <onuertey1997@gmail.com>
2023-10-12 13:51:27 +0000

    feat: completed first phase of projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -17,7 +17,7 @@ export default ():JSX.Element => {
                 <PageIntroDescription cssSectionClass="project-section" cssSectionId="" />
                 <section className="project-section" id="projects-repo">
                     <figure className="project-card flex flex-col rounded-lg w-52 h-auto shadow-lg shadow-black">
-                        <img className="img-tag w-full rounded-lg rounded-br-none rounded-bl-none" src={myImage} alt="me" />
+                        <img className="img-tag w-full rounded-lg rounded-br-none rounded-bl-none" src={myImage} alt="me" loading="lazy" />
                         <figcaption className="image-caption grid grid-rows-2 grid-cols-1 place-content-center gap-1">
                             <div className="caption-text grid grid-cols-2 grid-rows-1 place-conent-center border-b border-gray-300 py-2" id="caption-links">
                                 <div className="caption-link text-center" id="get-code"><a href="" rel="noreferrer">lskjdlkfj</a></div>
