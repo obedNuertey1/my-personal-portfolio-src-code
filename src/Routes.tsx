b93a7899906dbfdbb3c45ff4d9889156f9c5b61f
Obed Nuertey <onuertey1997@gmail.com>
2023-10-13 23:19:24 +0000

    fix: refactored Home.tsx code

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,6 @@
 import {Routes, Route} from "react-router-dom";
 import {ComponentType, lazy} from "react";
 import PageLayoutComponent from "./components/pageLayoutComponent";
-// import Home from "./pages/Home";
-// import Articles from "./pages/Articles";
-// import Projects from "./pages/Projects";
-// import Work from "./pages/Work";
 
 const Home:any = lazy(async ():Promise<{default: ComponentType<JSX.Element>}>=>{
     return await (import("./pages/Home"));
