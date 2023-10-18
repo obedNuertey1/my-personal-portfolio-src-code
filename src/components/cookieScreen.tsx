017eded69e0e1e9b1cf32c1a64ea53272162b192
Obed Nuertey <onuertey1997@gmail.com>
2023-10-18 22:07:14 +0000

    feat: added some new features

--- src/components/cookieScreen.tsx
+++ src/components/cookieScreen.tsx
@@ -1,18 +1,44 @@
+import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCookieBite } from "@fortawesome/free-solid-svg-icons";
+import $ from "jquery";
+
+
 export default ():JSX.Element=>{
 
+    useEffect(()=>{
+        $("dialog").hide();
+        const dialog:any = document.querySelector("dialog");
+        dialog.close();
+    },[]);
+
+    const handleAcceptCookie = ():void=>{
+        document.cookie = "myCookie=obednuerteyportfolioxyz; path=/";
+        $("dialog").hide();
+        const dialog:any = document.querySelector("dialog");
+        dialog.close();
+    };
+
+    const handleRegectCookie = ():void=>{
+        $("dialog").hide();
+        const dialog:any = document.querySelector("dialog");
+        dialog.close();
+    };
+
     return (
-        <>
-            <div data-testid="cookie" id="cookie" className="cookie text-xxs p-5 h-auto bg-white flex flex-col justify-center items-center gap-5 backdrop:bg-gray-900 backdrop-blur-sm backdrop-opacity-20 rounded-lg border border-black border-solid max-w-[60ch] mx-auto z-40">
-                <article className="text-center z-30">
-                    <p>Hi there ✋😁!</p>
-                    <p>This site uses cookies to enhance user experience.</p>
-                    <p className="mt-1">Would you like to enable cookies?</p>
-                </article>
-                <article className="flex flex-row gap-2 flex-nowrap">
-                    <button className="border-green-600 border-2 rounded-md p-1 bg-transparent relative cookie-green-button z-30">I Accept Cookies!</button>
-                    <button className="border-red-600 border-2 rounded-md p-1 bg-transparent relative cookie-red-button z-30">I Reject Cookies!</button>
-                </article>
-            </div>
-        </>
+        <dialog data-testid="cookie" id="cookie" className="cookie text-xxs p-5 h-auto max-h-fit bg-white flex flex-col justify-center items-center gap-5 rounded-lg max-w-[60ch] mx-auto z-40">
+            <article className="text-center z-30">
+                <p className="text-lg font-extrabold">Cookies!<span className="text-amber-700 stroke-black fill-black text-xl"><FontAwesomeIcon icon={faCookieBite} /></span></p>
+                <p>Hi there ✋😁!</p>
+                <p>This site uses cookies to enhance user experience.</p>
+                <p>For instance the yellow introductory page that greeted you is controlled by cookies</p>
+                <p>When cookie is accepted it won't show upon reload. However, when cookie is rejected it shows up anytime the page reloads</p>
+                <p className="mt-1">Would you like to enable cookies?</p>
+            </article>
+            <article className="flex flex-row gap-2 flex-nowrap">
+                <button onClick={handleAcceptCookie} className="border-green-600 border-2 rounded-md p-1 bg-transparent relative cookie-green-button z-30">I Accept Cookies!</button>
+                <button onClick={handleRegectCookie} className="border-red-600 border-2 rounded-md p-1 bg-transparent relative cookie-red-button z-30">I Reject Cookies!</button>
+            </article>
+        </dialog>
     );
 }
--- src/components/pageLayoutComponent.tsx
+++ src/components/pageLayoutComponent.tsx
@@ -4,13 +4,40 @@ import { Outlet } from "react-router-dom";
 import CircleWithBars from "./loadingComponent";
 import {Suspense, useEffect} from "react";
 import PhoneNav from "./phoneNav";
+import CookieScreen from "./cookieScreen";
+import $ from "jquery";
+
 
 export default ():JSX.Element=>{
+
+
     useEffect(()=>{
-        document.cookie = "myCookie=obednuerteyportfolioxyz; path=/";
+
+        $("dialog").hide();
+        const dialog:any = document.querySelector("dialog");
+        dialog.close();
+
+        (()=>{
+
+            if(document.cookie != "myCookie=obednuerteyportfolioxyz"){
+                setTimeout(()=>{
+                    setTimeout(()=>{
+                        const dialog:any = document.querySelector("dialog");
+                        dialog.showModal();
+                        $("dialog").show();
+                    }, 3000);
+                }, 3000);
+            }else{
+                const dialog:any = document.querySelector("dialog");
+                dialog.close();
+                $("dialog").hide();
+            }
+
+        })();
     });
     return (
-        <div className="oblivion relative">
+        <div className="oblivion relative" >
+            <CookieScreen />
             <PhoneNav className="mx-auto fixed z-10 font-tahoma w-full p-2 fade-in px-2 bottom-0 shadow-lg bg-white rounded-t-lg opacity-95 phone-nav sm:hidden" />
             < HeaderComponent />
                 <main className="mx-auto font-tahoma max-w-[80ch] p-5 fade-in px-4">
