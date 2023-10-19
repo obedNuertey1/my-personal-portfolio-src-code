94ef13b03e975fe6b1849b7a696c708b34bc6369
Obed Nuertey <onuertey1997@gmail.com>
2023-10-19 16:39:33 +0000

    fix: refactored Home.tsx for better maintainability

--- src/assets/images/project-info.ts
+++ src/assets/images/project-info.ts
@@ -1,6 +1,17 @@
 import projectImage1 from "./large/Hacker-PNG-Image_2.png";
 import projectImage2 from "./large/maxicoffee.jpg";
 import projectImage3 from "./large/registration-form.png";
+import projectImage4 from "./large/tailwind-color-marker.png";
+import projectImage5 from "./large/visulizing-game-data.png";
+import projectImage6 from "./large/heat-map.jpg";
+import projectImage7 from "./large/Bachelors-chloropleth-map.png";
+import projectImage8 from "./large/race-doping-visualization.png";
+import projectImage9 from "./large/us-gdp.png";
+import projectImage10 from "./large/tailwind-photo-app.png";
+import projectImage11 from "./large/calculator-app.png";
+import projectImage12 from "./large/pomodoro-timer.png";
+import projectImage13 from "./large/markdonw-to-html-generator.png";
+import projectImage14 from "./large/drum-machine.png";
 
 
 export const projectDetails =
@@ -8,4 +19,15 @@ export const projectDetails =
         {captionGistText: "portfolio", image: {src: projectImage1, alt: "portfolio"}, visitPageLink: "", getCodeLink:""},
         {captionGistText: "tailwind coffee menu", image: {src: projectImage2, alt: "coffee menu"}, visitPageLink: "https://maxicoffee.netlify.app/", getCodeLink:"https://github.com/obedNuertey1/coffee-menu-react-typescript-tailwind.git"},
         {captionGistText: "tailwind form", image: {src: projectImage3, alt: "tailwind form"}, visitPageLink: "https://tailwindcss-registration-form.netlify.app", getCodeLink:"https://github.com/obedNuertey1/tailwindcss-registration-form.git"},
+        {captionGistText: "tailwind color slider", image: {src: projectImage4, alt: "tailwind color marker"}, visitPageLink: "https://tailwindcss-color-slider.netlify.app/", getCodeLink:"https://github.com/obedNuertey1/css-color-slider.git"},
+        {captionGistText: "visualizing game data", image: {src: projectImage5, alt: "video game data"}, visitPageLink: "https://visualizing-video-game-sales-data.netlify.app", getCodeLink:""},
+        {captionGistText: "weather heat map", image: {src: projectImage6, alt: "heat map"}, visitPageLink: "https://visualizing-weather-data.netlify.app/", getCodeLink:""},
+        {captionGistText: "bachelors degree chloropleth", image: {src: projectImage7, alt: "chloropleth"}, visitPageLink: "https://us-bachelor-attainment-map.netlify.app/", getCodeLink:""},
+        {captionGistText: "doping data visualization", image: {src: projectImage8, alt: "scatterplot"}, visitPageLink: "https://race-doping-visualization.netlify.app/", getCodeLink:""},
+        {captionGistText: "gdp data visualization", image: {src: projectImage9, alt: "histogram"}, visitPageLink: "https://usa-gdp.netlify.app/", getCodeLink:""},
+        {captionGistText: "tailwind kitty photo app", image: {src: projectImage10, alt: "photo-grid"}, visitPageLink: "https://tailwindcss-flexbox.netlify.app/", getCodeLink:""},
+        {captionGistText: "calculator app", image: {src: projectImage11, alt: "calculator"}, visitPageLink: "https://obn-calculator-app.netlify.app/", getCodeLink:""},
+        {captionGistText: "pomodoro timer app", image: {src: projectImage12, alt: "timer"}, visitPageLink: "https://obn-pomodoro-timer.netlify.app/", getCodeLink:""},
+        {captionGistText: "markdown to html generator", image: {src: projectImage13, alt: "markdown-editor"}, visitPageLink: "https://markdown-gen.netlify.app/", getCodeLink:""},
+        {captionGistText: "drum machine", image: {src: projectImage14, alt: "drum-machine"}, visitPageLink: "https://obn-drum-machine.netlify.app/", getCodeLink:""},
     ]
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,7 +27,7 @@ const articles:any = [
     {time: "20/8/2023", $h3: "😎🤯Zoom Feature You Might Not Know About As A Newbie Developer", $href: "https://dev.to/obednuertey1/zoom-feature-you-might-not-know-about-as-a-newbie-developer-1437"},
 ];
 
-const SecondSectionComponent = ({articles}:any):JSX.Element=>(
+const ArticleSectionComponent = ({articles}:any):JSX.Element=>(
     <section id="article" className="homepage-section pb-12">
         <h2 id="article-header" className="article-section text-sm mb-12 font-bold text-blue-900">RECENT ARTICLES <img src="https://img.shields.io/badge/dev.to-0A0A0A?style=for-the-badge&logo=dev.to&logoColor=white" alt="dev.to" style={{width: "100px", display: "inline-block"}}/></h2>
         <div id="article-body" className="article-section">
@@ -37,17 +37,17 @@ const SecondSectionComponent = ({articles}:any):JSX.Element=>(
     </section>
 );
 
-const ThirdSectionComponent = ():JSX.Element=>(
+const ProjectsSectionComponent = ():JSX.Element=>(
     <section id="projects" className="homepage-section pt-12 relative">
         <div onClick={handleLeftIframeNav} className="iframe-nav absolute w-auto h-1/2 bottom-1/4 left-0 bg-gray-200 opacity-50 py-32 px-2 text-4xl text-gray-700 rounded-md"><p className="text-center"><FontAwesomeIcon icon={faArrowCircleLeft} /></p></div>
         <div onClick={handleRightIframeNav} className="iframe-nav absolute w-auto h-1/2 bottom-1/4 right-0 bg-gray-200 opacity-50 py-32 text-4xl text-gray-700 px-2 rounded-md"><p className="text-center"><FontAwesomeIcon icon={faArrowCircleRight} /></p></div>
         <h2 className="project" id="project-head"></h2>
         <div id="show-projects" className="project flex flex-col sm:flex-row overflow-y-auto sm:overflow-x-auto max-w-w100 mx-auto select-none">
             <div className="current-projects">
-                <iframe  className="rounded-md shadow-md" src="https://merry-creponne-b3b2ec.netlify.app/" title="my-cats" allowFullScreen frameBorder={0} sandbox="allow-same-origin allow-scripts"></iframe>
+                <iframe  className="rounded-md shadow-md" src="https://obn-calculator-app.netlify.app/" title="calculator" allowFullScreen frameBorder={0} sandbox="allow-same-origin allow-scripts"></iframe>
             </div>
             <div id="current-projects-2" className="current-projects">
-                <iframe src="https://incomparable-jalebi-1f7abb.netlify.app/" className="rounded-md shadow-md" title="my-cats" allowFullScreen frameBorder={0} sandbox="allow-same-origin allow-scripts"></iframe>
+                <iframe src="https://obn-pomodoro-timer.netlify.app/" className="rounded-md shadow-md" title="pomodoro" allowFullScreen frameBorder={0} sandbox="allow-same-origin allow-scripts"></iframe>
             </div>
         </div>
         <Link to="/projects" className="article-section font-thin text-gray-600 hover:shadow-md hover:bg-gray-600 hover:text-white p-1 rounded text-sm select-none"><span className="mr-1 hover:mr-3">more</span> <FontAwesomeIcon icon={faArrowRight} /></Link>
@@ -85,8 +85,8 @@ export default ():JSX.Element =>{
             </Helmet>
             <div data-testid="home-section" id="home-section" className="page">
                 <PageIntroDescription cssSectionClass="homepage-section" cssSectionId="introduction" heading={intro.heading} paragraph={intro.paragraph} />
-                <SecondSectionComponent articles={articles} />
-                <ThirdSectionComponent />
+                <ArticleSectionComponent articles={articles} />
+                <ProjectsSectionComponent />
             </div>
         </>
     );
